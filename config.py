5bdc871d498456656b137c08f9e032b6a27bad50
sanmart02 <113845440+sanmart02@users.noreply.github.com>
2022-11-29 13:00:31 -0300

    Update config.py

--- config.py
+++ config.py
@@ -46,8 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Oi {mention}\n\nVocê precisa se inscrever no @hubdatv para conseguir acessar os arquivos do canal de onde veio.
-")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Oi {mention}\n\nVocê precisa se inscrever no @hubdatv para conseguir acessar os arquivos do canal de onde veio.")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
