46e19bcfa53b517228e371ac07ccb1fcb2546c9a
sanglobo <122318943+sanglobo@users.noreply.github.com>
2023-01-09 19:52:18 -0300

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1170822786"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hubdatv:hubdatv@filesharebot.006q6du.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharebot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hubdatv:hubdatv@cluster0.bxuwdnz.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "sessaogloboplay")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001713719112"))
